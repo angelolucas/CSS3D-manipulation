c12fc6546738b5c9778ce5d5ef2e1ae3406f54c7
Ângelo Lucas <angelo.lrc@gmail.com>
2017-08-07 22:24:37 -0300

    manipuling transform-origin

--- javascript/main.js
+++ javascript/main.js
@@ -9,13 +9,23 @@ var control = {
   scaleY: 1,
   scaleZ: 1,
   scale3D: 1,
+  originX: 50,
+  originY: 50,
+  originZ: -50
 };
 
+var property = {
+  transform: null,
+  transformOrigin: null
+}
+
 var element = document.querySelector('.coca-cola');
-var printResult = document.querySelector('.print-result .value');
+var printTransform = document.querySelector('.print-result .transform');
+var printTransformOrigin = document.querySelector('.print-result .transform-origin');
+var axes = document.querySelector('.axes');
 
 var transform = function() {
-  var value =
+  property.transform =
     'translateX(' + control.translateX + 'px) ' +
     'translateY(' + control.translateY + 'px) ' +
     'translateZ(' + control.translateZ + 'px) ' +
@@ -31,57 +41,96 @@ var transform = function() {
       control.scale3D.toFixed(3) +
     ')';
 
-  element.setAttribute('style', 'transform: ' + value);
-
-  printResult.innerHTML = value;
+  applyTransform();
 };
 
+var transformOrigin = function() {
+  property.transformOrigin =
+    control.originX + '% ' + control.originY + '% ' + control.originZ + 'px'
+
+  axes.setAttribute('style',
+    'top: ' + control.originY + '%;' +
+    'left: ' + control.originX + '%;' +
+    'transform: translate3d(50%, 50%, ' + control.originZ + 'px);'
+  );
+
+  applyTransform();
+};
+
+var applyTransform = function() {
+  element.setAttribute('style',
+    'transform: ' + property.transform + ';' +
+    'transform-origin: ' + property.transformOrigin + ';'
+  );
+
+  printTransform.innerHTML = property.transform;
+  printTransformOrigin.innerHTML = property.transformOrigin;
+}
+
 // Init GUI
-var gui = new dat.GUI();
+var GUI = new dat.GUI({width: 400});
+
+// GUI Transform Origin
+var GUITransformOrigin = GUI.addFolder('transform-origin');
+GUITransformOrigin.add(control, 'originX', -600, 600).step(1).onChange(function(){
+  transformOrigin();
+});
+GUITransformOrigin.add(control, 'originY', -100, 200).step(1).onChange(function(){
+  transformOrigin();
+});
+GUITransformOrigin.add(control, 'originZ', -100, 100).step(1).onChange(function(){
+  transformOrigin();
+});
+
+var GUITransform = GUI.addFolder('transform');
 
 // GUI Translate
-var translate = gui.addFolder('translate');
-translate.add(control, 'translateX', -100, 100).step(1).onChange(function(){
+var GUITranslate = GUITransform.addFolder('translate');
+GUITranslate.add(control, 'translateX', -100, 100).step(1).onChange(function(){
   transform();
 });
-translate.add(control, 'translateY', -100, 100).step(1).onChange(function(){
+GUITranslate.add(control, 'translateY', -100, 100).step(1).onChange(function(){
   transform();
 });
-translate.add(control, 'translateZ', -100, 100).step(1).onChange(function(){
+GUITranslate.add(control, 'translateZ', -100, 100).step(1).onChange(function(){
   transform();
 });
 
 // GUI Rotate
-var rotate = gui.addFolder('rotate');
+var GUIRotate = GUITransform.addFolder('rotate');
 
-rotate.add(control, 'rotateX', -180, 180).step(1).onChange(function(){
+GUIRotate.add(control, 'rotateX', -180, 180).step(1).onChange(function(){
   transform();
 });
-rotate.add(control, 'rotateY', -180, 180).step(1).onChange(function(){
+GUIRotate.add(control, 'rotateY', -180, 180).step(1).onChange(function(){
   transform();
 });
-rotate.add(control, 'rotateZ', -180, 180).step(1).onChange(function(){
+GUIRotate.add(control, 'rotateZ', -180, 180).step(1).onChange(function(){
   transform();
 });
 
 // GUI Scale
-var scale = gui.addFolder('scale');
-scale.add(control, 'scale3D', 0, 2).onChange(function(){
+var GUIScale = GUITransform.addFolder('scale');
+GUIScale.add(control, 'scale3D', 0, 2).onChange(function(){
   transform();
 });
-scale.add(control, 'scaleX', 0, 2).onChange(function(){
+GUIScale.add(control, 'scaleX', 0, 2).onChange(function(){
   transform();
 });
-scale.add(control, 'scaleY', 0, 2).onChange(function(){
+GUIScale.add(control, 'scaleY', 0, 2).onChange(function(){
   transform();
 });
-scale.add(control, 'scaleZ', 0, 2).onChange(function(){
+GUIScale.add(control, 'scaleZ', 0, 2).onChange(function(){
   transform();
 });
 
 // Open GUI Folders
-rotate.open();
-translate.open();
-scale.open();
 
-window.onload = transform();
+GUIRotate.open();
+GUITranslate.open();
+GUIScale.open();
+
+window.onload = function() {
+  transform();
+  transformOrigin();
+}
