a90f965e07001351e6b6521e9066e1e38652787f
Ângelo Lucas <angelo.lrc@gmail.com>
2017-08-21 22:25:12 -0300

    improving composition

--- javascript/main.js
+++ javascript/main.js
@@ -5,20 +5,30 @@ var selector = {
   printResult: document.querySelector('.print-result')
 };
 
-var control = {
-  rotateX: 0,
-  rotateY: 0,
-  rotateZ: 0,
-  translateX: 0,
-  translateY: 0,
-  translateZ: 0,
-  scaleX: 1,
-  scaleY: 1,
-  scaleZ: 1,
-  scale3D: 1,
-  originX: 50,
-  originY: 50,
-  originZ: -50,
+var value = {
+  transform: {
+    rotate: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    translate: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    scale: {
+      xyz: 1,
+      x: 1,
+      y: 1,
+      z: 1,
+    }
+  },
+  transformOrigin: {
+    x: 50,
+    y: 50,
+    z: -50
+  },
   perspective: 400
 };
 
@@ -31,19 +41,19 @@ var property = {
 var transform = function() {
   property.transform =
     'transform: ' +
-      'rotateX(' + control.rotateX + 'deg) ' +
-      'rotateY(' + control.rotateY + 'deg) ' +
-      'rotateZ(' + control.rotateZ + 'deg) ' +
-      'translateX(' + control.translateX + 'px) ' +
-      'translateY(' + control.translateY + 'px) ' +
-      'translateZ(' + control.translateZ + 'px) ' +
-      'scaleX(' + control.scaleX.toFixed(3) + ') ' +
-      'scaleY(' + control.scaleY.toFixed(3) + ') ' +
-      'scaleZ(' + control.scaleZ.toFixed(3) + ') ' +
+      'rotateX(' + value.transform.rotate.x + 'deg) ' +
+      'rotateY(' + value.transform.rotate.y + 'deg) ' +
+      'rotateZ(' + value.transform.rotate.z + 'deg) ' +
+      'translateX(' + value.transform.translate.x + 'px) ' +
+      'translateY(' + value.transform.translate.y + 'px) ' +
+      'translateZ(' + value.transform.translate.z + 'px) ' +
+      'scaleX(' + value.transform.scale.x.toFixed(3) + ') ' +
+      'scaleY(' + value.transform.scale.y.toFixed(3) + ') ' +
+      'scaleZ(' + value.transform.scale.z.toFixed(3) + ') ' +
       'scale3d(' +
-        control.scale3D.toFixed(3) + ', ' +
-        control.scale3D.toFixed(3) + ', ' +
-        control.scale3D.toFixed(3) +
+        value.transform.scale.xyz.toFixed(3) + ', ' +
+        value.transform.scale.xyz.toFixed(3) + ', ' +
+        value.transform.scale.xyz.toFixed(3) +
       ');';
 
   applyTransform();
@@ -52,12 +62,12 @@ var transform = function() {
 var transformOrigin = function() {
   property.transformOrigin =
     'transform-origin: ' +
-      control.originX + '% ' + control.originY + '% ' + control.originZ + 'px;';
+      value.transformOrigin.x + '% ' + value.transformOrigin.y + '% ' + value.transformOrigin.z + 'px;';
 
   selector.axes.setAttribute('style',
-    'top: ' + control.originY + '%;' +
-    'left: ' + control.originX + '%;' +
-    'transform: translate3d(50%, 50%, ' + control.originZ + 'px);'
+    'top: ' + value.transformOrigin.y + '%;' +
+    'left: ' + value.transformOrigin.x + '%;' +
+    'transform: translate3d(50%, 50%, ' + value.transformOrigin.z + 'px);'
   );
 
   applyTransform();
@@ -73,7 +83,7 @@ var applyTransform = function() {
 };
 
 var perspective = function() {
-  property.perspective = 'perspective: ' + control.perspective + 'px;';
+  property.perspective = 'perspective: ' + value.perspective + 'px;';
 
   selector.objectContainer.setAttribute('style', property.perspective);
 
@@ -94,13 +104,13 @@ var GUI = new dat.GUI({width: 300});
 var GUITransformOrigin = GUI.addFolder('transform-origin');
 GUITransformOrigin.open();
 
-GUITransformOrigin.add(control, 'originX', -600, 600).step(1).onChange(function(){
+GUITransformOrigin.add(value.transformOrigin, 'x', -600, 600).step(1).onChange(function(){
   transformOrigin();
 });
-GUITransformOrigin.add(control, 'originY', -100, 200).step(1).onChange(function(){
+GUITransformOrigin.add(value.transformOrigin, 'y', -100, 200).step(1).onChange(function(){
   transformOrigin();
 });
-GUITransformOrigin.add(control, 'originZ', -100, 100).step(1).onChange(function(){
+GUITransformOrigin.add(value.transformOrigin, 'z', -100, 100).step(1).onChange(function(){
   transformOrigin();
 });
 
@@ -111,13 +121,13 @@ GUITransform.open();
 var GUIRotate = GUITransform.addFolder('rotate');
 GUIRotate.open();
 
-GUIRotate.add(control, 'rotateX', -180, 180).step(1).onChange(function(){
+GUIRotate.add(value.transform.rotate, 'x', -180, 180).step(1).onChange(function(){
   transform();
 });
-GUIRotate.add(control, 'rotateY', -180, 180).step(1).onChange(function(){
+GUIRotate.add(value.transform.rotate, 'y', -180, 180).step(1).onChange(function(){
   transform();
 });
-GUIRotate.add(control, 'rotateZ', -180, 180).step(1).onChange(function(){
+GUIRotate.add(value.transform.rotate, 'z', -180, 180).step(1).onChange(function(){
   transform();
 });
 
@@ -125,13 +135,13 @@ GUIRotate.add(control, 'rotateZ', -180, 180).step(1).onChange(function(){
 var GUITranslate = GUITransform.addFolder('translate');
 GUITranslate.open();
 
-GUITranslate.add(control, 'translateX', -100, 100).step(1).onChange(function(){
+GUITranslate.add(value.transform.translate, 'x', -100, 100).step(1).onChange(function(){
   transform();
 });
-GUITranslate.add(control, 'translateY', -100, 100).step(1).onChange(function(){
+GUITranslate.add(value.transform.translate, 'y', -100, 100).step(1).onChange(function(){
   transform();
 });
-GUITranslate.add(control, 'translateZ', -100, 100).step(1).onChange(function(){
+GUITranslate.add(value.transform.translate, 'z', -100, 100).step(1).onChange(function(){
   transform();
 });
 
@@ -139,16 +149,16 @@ GUITranslate.add(control, 'translateZ', -100, 100).step(1).onChange(function(){
 var GUIScale = GUITransform.addFolder('scale');
 GUIScale.open();
 
-GUIScale.add(control, 'scale3D', -1, 2).onChange(function(){
+GUIScale.add(value.transform.scale, 'xyz', -1, 2).onChange(function(){
   transform();
 });
-GUIScale.add(control, 'scaleX', -1, 2).onChange(function(){
+GUIScale.add(value.transform.scale, 'x', -1, 2).onChange(function(){
   transform();
 });
-GUIScale.add(control, 'scaleY', -1, 2).onChange(function(){
+GUIScale.add(value.transform.scale, 'y', -1, 2).onChange(function(){
   transform();
 });
-GUIScale.add(control, 'scaleZ', -1, 2).onChange(function(){
+GUIScale.add(value.transform.scale, 'z', -1, 2).onChange(function(){
   transform();
 });
 
@@ -156,7 +166,7 @@ GUIScale.add(control, 'scaleZ', -1, 2).onChange(function(){
 var GUIPerspective = GUI.addFolder('perspective');
 GUIPerspective.open();
 
-GUIPerspective.add(control, 'perspective', 100, 1000).step(1).onChange(function(){
+GUIPerspective.add(value, 'perspective', 100, 1000).step(1).onChange(function(){
   perspective();
 });
 
